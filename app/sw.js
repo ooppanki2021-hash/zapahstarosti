241e468261dcd50e1b35690afeb9d211b564c9c3
ooppanki2021-hash <ooppanki2021@gmail.com>
2026-09-17 00:11:03 +0300

    Add files via upload

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mechta-v2';
+const CACHE = 'mechta-v3';
 const FILES = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
